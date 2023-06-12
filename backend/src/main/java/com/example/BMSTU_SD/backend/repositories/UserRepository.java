f787a8f1b5e0a34a4a9b753e12020cbc965f7894
Елизавета Ослина <104625209+shevdis@users.noreply.github.com>
2023-06-12 16:31:12 +0300

    lab78
    lab78

--- backend/src/main/java/com/example/BMSTU_SD/backend/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.BMSTU_SD.backend.repositories;
-
-import com.example.BMSTU_SD.backend.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
